3e948983c5abc4620fe99cacc702d97c06f60312
Neeta Veer <132153429+neetaveer99@users.noreply.github.com>
2023-08-04 11:41:58 +0530

    Add files via upload

---
+++ src/reducers/TodoReducers.js
@@ -0,0 +1,13 @@
+export const TodoReducers = (state = { todos: [] }, action ) => {
+    switch (action.type) {
+        case "ADD_TODO":
+            return { todos: action.payload };
+
+            case "REMOVE_TODO":
+                return { todos : action.payload };
+
+                default :
+                return state;
+
+    }
+};
